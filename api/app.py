f9c028533fd40a50e3aba30967e798da07db8c53
lIANYI <lc923@imperial.ac.uk>
2023-11-30 16:30:06 +0000

    update

--- api/app.py
+++ api/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
 
-
 app = Flask(__name__)
 
 
 API_KEYS = {"opentripmap": "5ae2e3f221c38a28845f05b6f0cdf9cd4ed80f90f5ddfc9ebf916642"}
 
 
-SEATGEEK_CLIENT_ID = 'Mzg2MDA4NDd8MTcwMTI1MzQ0Mi42NzI0ODE4'
+SEATGEEK_CLIENT_ID = "Mzg2MDA4NDd8MTcwMTI1MzQ0Mi42NzI0ODE4"
 
 
-SEATGEEK_CLIENT_SECRET = '42ab0a3837f94f19b3bdcf5f0ba4192c5c399491e49bcb78dd39852ab61c698b'
+SEATGEEK_CLIENT_SECRET = (
+    "42ab0a3837f94f19b3bdcf5f0ba4192c5c399491e49bcb78dd39852ab61c698b"
+)
 
 
 BASE_URLS = {"opentripmap": "https://api.opentripmap.com"}
@@ -20,15 +21,18 @@ BASE_URLS = {"opentripmap": "https://api.opentripmap.com"}
 def get_seatgeek_events(city, date):
     base_url = "https://api.seatgeek.com/2/events"
     params = {
-        'client_id': SEATGEEK_CLIENT_ID,
-        'client_secret': SEATGEEK_CLIENT_SECRET,
-        'venue.city': city,
-        'datetime_local.gte': date,  # Assuming you want events from this date forward
-        'datetime_local.lte': date   # If you want events only on this date, set this too
+        "client_id": SEATGEEK_CLIENT_ID,
+        "client_secret": SEATGEEK_CLIENT_SECRET,
+        "venue.city": city,
+        "datetime_local.gte": date,
     }
+
     response = requests.get(base_url, params=params)
+
     if response.ok:
-        return response.json(), None
+        data = response.json()
+        events_data = data.get("events", [])
+        return events_data, None
     else:
         return None, "Error fetching events from SeatGeek"
 
@@ -71,16 +75,16 @@ def get_city_info():
     city = request.args.get("city")
     date = request.args.get("date")
 
-
     places_data, places_error = get_places_data(city)
     events_data, events_error = get_seatgeek_events(city, date)
-
+    print("Events data:", events_data)
     if places_error or events_error:
         error_message = places_error or events_error
         return jsonify({"error": error_message}), 500
 
-    
-    return render_template("results.html", places_data=places_data, events_data=events_data['events'])
+    return render_template(
+        "results.html", places_data=places_data, events_data=events_data
+    )
 
 
 if __name__ == "__main__":
