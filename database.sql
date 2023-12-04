98d6b885396fbfe570d62a5330d3820621d31399
Alva Si <alva.si19@imperial.ac.uk>
2023-12-04 15:15:13 +0000

    changed database schema

--- database.sql
+++ database.sql
@@ -4,8 +4,9 @@ CREATE TABLE userdata (
     country VARCHAR(50),
     city VARCHAR(50),
     exploration_date DATE
+    attraction_type VARCHAR(50)
 );
 
-INSERT INTO userdata (username, country, city, exploration_date) VALUES ('exampleuser', 'examplecountry', 'examplecity', '2023-01-01');
+INSERT INTO userdata (username, country, city, exploration_date, attraction_type) VALUES ('exampleuser', 'examplecountry', 'examplecity', '2023-01-01', 'exampletype');
 
 SELECT * FROM userdata;
