f0301273335b388671f668d921d6a722290c6c6c
Zhang Zhang <zz3823@imperial.ac.uk>
2023-12-05 17:32:32 +0000

    initialise golden hour

--- api/app.py
+++ api/app.py
@@ -181,7 +181,7 @@ def get_city_info():
     # Weather
     min_temp = max_temp = None
     weather_data = {}  # Initialize weather_data as an empty dictionary
-    sunrise_time = sunset_time = None  # Initialize sunrise and sunset times
+    sunrise_time = sunset_time = golden_hour_time =  None  # Initialize sunrise, sunset, and golden hour times
 
     if places_data and "features" in places_data:
         location = places_data["features"][0]["geometry"]["coordinates"]
