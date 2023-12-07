2fb2ab882fba9b6fc19980fd5c2c16696df7ecd0
Alva Si <alva.si19@imperial.ac.uk>
2023-12-07 13:35:52 +0000

    keep same route

--- api/app_test.py
+++ api/app_test.py
@@ -59,52 +59,6 @@ class AppTestCase(unittest.TestCase):
         self.assertIn(b"Upcoming Events", response.data)
         self.assertIn(b"Weather", response.data)
 
-    @patch("app.get_db_connection")
-    @patch("app.get_places_data")
-    @patch("app.get_place_information")
-    @patch("app.get_dining_data")
-    @patch("app.get_seatgeek_events")
-    @patch("app.get_weather_data")
-    @patch("app.get_airquality_forecast_data")
-    def test_weather_info_page(
-        self,
-        mock_weather_data,
-        mock_airquality_forecast_data,
-        mock_events_data,
-        mock_dining_data,
-        mock_place_information,
-        mock_places_data,
-        mock_db_connection,
-    ):
-        mock_db_connection.return_value = MagicMock()
-
-        mock_places_data.return_value = ([], 0, 0, None)
-        mock_dining_data.return_value = ([], 0, 0, None)
-        mock_events_data.return_value = ([], None)
-        mock_weather_data.return_value = (
-            {"temperature": {"min": 280, "max": 285}},
-            None,
-        )
-        mock_airquality_forecast_data.return_value = (
-            [{"time": 1701864000, "avg_aqi": 2.3333333333333335}, ...],  # Example data
-            None,
-        )
-        mock_place_information.return_value = (
-            "Example Description",
-            ["https://example.com"],
-            "example_instagram",
-            "example_twitter",
-            "example_facebook",
-            None,
-        )
-        response = self.app.get(
-            "/get-weather-info?city=London&username=test&country=UK&date=2023-12-31"
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Discover", response.data)
-        self.assertIn(b"Upcoming Events", response.data)
-        self.assertIn(b"Weather", response.data)
-
 
 if __name__ == "__main__":
     unittest.main()
